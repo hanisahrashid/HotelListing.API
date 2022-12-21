1500428b60103c73a5c83136b5bc4b7ad3262933
Hanisah Rashid <hanisah.abdrashid@gmail.com>
2022-12-21 17:15:27 +0800

    Add AutoMapper and Repositories

---
+++ HotelListing.API/Models/Country/ViewCountryDto.cs
@@ -0,0 +1,12 @@
+﻿using HotelListing.API.Models.Hotel;
+
+namespace HotelListing.API.Models.Country
+{
+    public class ViewCountryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string shortName { get; set; }
+        public List<ViewHotelDto> Hotels { get; set; }
+    }
+}
---
+++ HotelListing.API/Models/Hotel/ViewHotelDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelListing.API.Models.Hotel
+{
+    public class ViewHotelDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+    }
+}
